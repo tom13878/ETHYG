4b4406ec83dab349cd83b38eb025f7b2ec19c704
Michiel van Dijk <michiel.vandijk@wur.nl>
2016-08-10 11:52:30 +0200

    Script to create unbalanced database for maize plots

---
+++ Code/Pool_data_ETH.R
@@ -0,0 +1,81 @@
+############################
+#### PREPARE ETH PANEL #####
+############################
+
+############################################
+############## READ THE DATA ###############
+############################################
+
+source("N:/Internationaal Beleid  (IB)/Projecten/2285000066 Africa Maize Yield Gap/SurveyData/Code\\ETH\\ETH_2011.r")
+source("N:/Internationaal Beleid  (IB)/Projecten/2285000066 Africa Maize Yield Gap/SurveyData/Code\\ETH\\ETH_2013.r")
+
+#######################################
+############## PACKAGES ETC ###########
+#######################################
+
+dataPath <- "N:/Internationaal Beleid  (IB)/Projecten/2285000066 Africa Maize Yield Gap/SurveyData/"
+wdPath <- "D:\\Data\\Projects\\ETHYG"
+setwd(wdPath)
+
+library(plyr)
+library(dplyr)
+library(ggplot2)
+library(stargazer)
+library(haven)
+library(tidyr)
+library(xtable)
+
+source("Code/winsor.R")
+
+options(scipen=999)
+
+###########################
+###### POOL DATABASE ######
+###########################
+
+# in the second wave, the household
+# identification number of the first
+# wave is used. However these are recorded
+# as an empty character string if the 
+# household entered the survey in wave
+# two. ("")
+
+table(ETH2013$household_id %in% "")
+ETH2013$household_id <- zap_empty(ETH2013$household_id)
+table(is.na(ETH2013$household_id))
+
+# the same is true of the individual id
+table(ETH2013$individual_id %in% "")
+ETH2013$individual_id <- zap_empty(ETH2013$individual_id )
+table(is.na(ETH2013$individual_id))
+
+# use the first wave household identification
+# number. Where this is missing use the
+# second wave household identification number
+ETH2013$household_id <- ifelse(is.na(ETH2013$household_id), ETH2013$household_id2, ETH2013$household_id)
+ETH2013$individual_id <- ifelse(is.na(ETH2013$individual_id), ETH2013$individual_id2, ETH2013$individual_id)
+
+# -------------------------------------
+# Some waves of the data have variables
+# that were not available in others.
+# -------------------------------------
+
+# get all name variables that are common to both waves
+good <- Reduce(intersect, list(names(ETH2011), names(ETH2013)))
+
+# select only those names common in both waves
+ETH2011_2 <- ETH2011[, good]
+ETH2013_2 <- ETH2013[, good]
+
+# new full dataset
+dbP <- rbind(ETH2011_2,ETH2013_2) %>%
+  select(hhid=household_id, indidy=individual_id, everything())
+
+#rm(good, path, ETH2011, ETH2011_2, ETH2013, ETH2013_2)
+
+# Select maize plots and head of household
+dbP <- filter(dbP, status %in% "HEAD", crop_code %in% 2)
+
+# Write file
+saveRDS(dbP, "Cache/Pooled_EHT.rds")
+
