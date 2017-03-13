6b87077caaf1317b64973313846340016a11b809
tom13878 <tomas.morley@wur.nl>
2017-03-13 17:06:31 +0100

    Endogeneity testing and first go at full liml model

--- Code/endogeneity_testing.R
+++ Code/endogeneity_testing.R
@@ -22,8 +22,9 @@ db1 <- filter(db1, lab < 2000, logN >= 0)
 db1$logNloglab <- db1$logN * db1$loglab
 db1$logslope <- log(db1$slope + 1)
 db1$agesq <- db1$age^2
-db1$dumoxen <- ifelse(db1$oxen > 0, 1, 0)
 db1$elevationsqt <- sqrt(db1$elevation)
+db1$SOC2sq <- db1$SOC2^2
+db1$GGD <- db1$GGD/1000
 
 # -------------------------------------
 # translog production function with 
@@ -34,35 +35,41 @@ db1$elevationsqt <- sqrt(db1$elevation)
 db1 <- as.data.frame(model.matrix(~ -1 + logyld +  logN + loglab  +
                       logNloglab + Pn + dist_market + sex + age + agesq +
                       ed_any + yesN + crop_count2 +
-                      logarea + impr + logslope + elevationsqt, data=db1))
-
-# for comparison we the following results we begin
-# with a simple translog function as a benchmark
-TL_simple <- lm(logyld ~ logN + loglab + 
-                     logNloglab + logarea +
-                  logslope + elevationsqt + crop_count2,
-                data=db1)
-
+                      logarea + impr + logslope + elevationsqt +
+                        SOC2 + SOC2sq + phdum55_2_70 + dumoxen + title +
+                        rain_wq + relprice + Pm + GGD +
+                        cost2large_town, data=db1))
+corrplot(cor(db1))
 
 # first stage linear model
-stage1_lm <- lm(logN ~ Pn + dist_market + age + agesq +
-                  logslope + elevationsqt + crop_count2, data=db1)
+stage1_lm <- lm(logN ~ relprice + sex + age + agesq +
+                  logslope + elevationsqt + crop_count2 +
+                  impr + ed_any + SOC2 +
+                   phdum55_2_70 + dist_market + GGD +
+                   cost2large_town, data=db1)
 
 # get residuals
 db1$v <- residuals(stage1_lm) 
 
+# try doing stepwise selection
+db2 <- select(db1, -Pn, -dist_market, -sex, -age, -agesq,
+                -ed_any, -yesN, -relprice, -Pm, -title, -SOC2sq,
+              -rain_wq, -cost2large_town) 
+null <- lm(logyld ~ logN + loglab + 
+             logNloglab + logarea, data=db2)
+full <- lm(logyld ~ ., data=db2)
+x <- step(null, scope=list(lower=null, upper=full), direction="forward")
+summary(lm(x$call$formula, data=db2))
+
 # second stage TL model with residuals
-stage2_TL_lm <- lm(logyld ~ logN + loglab + 
-                        logNloglab + logarea +
-                     logslope + elevationsqt + crop_count2 +
-                        v, data=db1)
+stage2_TL_lm <- lm(x$call$formula, data=db1)
 
 # bootstrap results - need to include ALL stages in bootstrap
 refit_TL_lm <- function(data, indx){
   dat <- data[indx,]
   fs <- lm(formula(stage1_lm), data=dat)
   dat$v <- residuals(fs) 
-  coef(lm(formula(stage2_TL_lm), data=dat))
+  coef(lm(x$call$formula, data=dat))
 }
 
 TL_lm_boot <- boot(db1, refit_TL_lm, R = 500)
@@ -77,8 +84,11 @@ p.value_lm <- round(2*pt(abs(t), df=df, lower=FALSE), 3)
 
 
 # first stage tobit model
-stage1_tob <- tobit(logN ~ Pn + dist_market + age + agesq +
-                      logslope + elevationsqt + crop_count2, data=db1)
+stage1_tob <- tobit(logN ~ relprice + sex + age + agesq +
+                      logslope + elevationsqt + crop_count2 +
+                      impr + ed_any + SOC2 +
+                      phdum55_2_70 + dist_market + GGD + 
+                      cost2large_town, data=db1)
 
 # calculate the generalized residual (Greene)
 d1 <- 1 - db1$yesN
@@ -89,18 +99,18 @@ mills <- -dnorm(-fitted(stage1_tob))/pnorm(-fitted(stage1_tob))
 db1$v <- d1 * mills + d2 * (theta * db1$logN - fitted(stage1_tob))
 
 # put v into the second stage TL model
-stage2_TL_tob <- lm(logyld ~ logN + loglab + 
-                      logNloglab + logarea + 
-                      logslope + elevationsqt + crop_count2 +
-                      v, data=db1)
+stage2_TL_tob <- lm(x$call$formula, data=db1)
 
 # bootstrap results: note that for some reason we cannot use the
 # formula(modl) trick with the tobit function -> have to write out everything
 # whole
 refit_TL_tob <- function(data, indx){
   dat <- data[indx, ]
-  fs <- tobit(logN ~ Pn + dist_market  + age + agesq +
-                logslope + elevationsqt + crop_count2, data=dat)
+  fs <- tobit(logN ~ relprice + sex + age + agesq +
+                logslope + elevationsqt + crop_count2 +
+                impr + ed_any + SOC2 +
+                phdum55_2_70 + dist_market + GGD +
+                cost2large_town, data=dat)
   d1 <- 1 - dat$yesN
   d2 <- dat$yesN
   sigma <- fs$scale
@@ -134,8 +144,11 @@ APE <- coef(stage1_tob) * scale_factor
 # bootstrap the making of the APE!!!
 refit_APE <- function(data, indx){
   dat <- data[indx, ]
-  fs <- tobit(logN ~ Pn + dist_market + age + agesq +
-                logslope + elevationsqt + crop_count2, data=dat)
+  fs <- tobit(logN ~ relprice + sex + age + agesq +
+                logslope + elevationsqt + crop_count2 +
+                impr + ed_any + SOC2 +
+                phdum55_2_70 + dist_market + GGD +
+                cost2large_town, data=dat)
   sigma <- fs$scale
   X <- model.matrix(fs)
   n <- nrow(X)
@@ -146,6 +159,13 @@ refit_APE <- function(data, indx){
 
 # results from bootstrap
 APE_boot <- boot(db1, refit_APE, R = 500)
+APE_SE <- summary(APE_boot)
+
+# work out p-values
+N <- nrow(db1)
+df <- N - length(APE)
+t <- APE/APE_SE[, 4]
+p.value_APE <- round(2*pt(abs(t), df=df, lower=FALSE), 3)
 
 # -------------------------------------
 # construct a table with the results
@@ -157,6 +177,18 @@ APE <- summary(APE_boot)
 FS_tab <- cbind(OLS[, 1:2], Tobit[-nrow(Tobit), 1:2], APE[, c(2, 4)])
 FS_tab <- round(FS_tab, 3)
 names(FS_tab) <- c("OLS", "SE", "Tobit", "SE", "APE", "BSE")
+
+# add stars to tables
+# ols first stage
+stars <- cut(OLS[, 4], breaks=c(0, 0.01, 0.05, 1),
+             labels=c("**", "*", ""), include.lowest = TRUE)
+FS_tab$OLS <- paste0(FS_tab$OLS, stars)
+
+# tobit first stage
+stars <- cut(Tobit[-nrow(Tobit), 4], breaks=c(0, 0.01, 0.05, 1),
+             labels=c("**", "*", ""), include.lowest = TRUE)
+FS_tab$Tobit <- paste0(FS_tab$Tobit, stars)
+
 
 # we can compare the lm and tobit first stage models to see which
 # fits better in terms of the R squared. However, wooldridge (2010)
@@ -173,25 +205,18 @@ FS_tab <- rbind(FS_tab, Rsq)
 row.names(FS_tab)[nrow(FS_tab)] <- "R-squared"
 
 # second stage table
-TL <- rbind(summary(TL_simple)$coef, v=NA)
 TL_lm <- summary(stage2_TL_lm)$coef
 TL_tob <- summary(stage2_TL_tob)$coef
 
 # bootstrapped SEs
 TL_lm_SE <- summary(TL_lm_boot)
 TL_tob_SE <- summary(TL_tob_boot)
-SS_tab <- as.data.frame(cbind(TL[, 1:2], TL_lm[, 1:2], TL_lm_SE[, 4],
+SS_tab <- as.data.frame(cbind(TL_lm[, 1:2], TL_lm_SE[, 4],
                               TL_tob[, 1:2], TL_tob_SE[, 4]))
 SS_tab <- round(SS_tab, 3)
-names(SS_tab) <- c("TL", "SE", "TL-LM", "SE", "BSE", "TL-Tob", "SE", "BSE")
+names(SS_tab) <- c("TL-LM", "SE", "BSE", "TL-Tob", "SE", "BSE")
 
 # add stars to tables
-# lm only
-stars <- cut(TL[, 4], breaks=c(0, 0.01, 0.05, 1),
-             labels=c("**", "*", ""), include.lowest = TRUE)
-stars[length(stars)] <- ""
-SS_tab$`TL` <- paste0(SS_tab$`TL`, stars)
-
 # lm -> translog
 stars <- cut(p.value_lm, breaks=c(0, 0.01, 0.05, 1),
              labels=c("**", "*", ""), include.lowest = TRUE)
