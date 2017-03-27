3e3951ce14762cf91a84b621b4be4deea10a028c
tom13878 <tomas.morley@wur.nl>
2017-03-27 10:07:24 +0200

    Changed ols parameters to SF ML parameters

--- Code/predict.sfa.R
+++ Code/predict.sfa.R
@@ -1,7 +1,13 @@
 predict.sfa <- function(sfa.model, data){
-  betas <- sfa.model$olsParam[-length(sfa.model$olsParam)]
+  data <- as.data.frame(data)
+  beta_names <- names(sfa.model$olsParam[-length(sfa.model$olsParam)])
+  betas <- sfa.model$mleParam[beta_names]
   form <- paste("~", paste(names(betas)[-1], collapse=" + "), " ")
   M <- model.matrix(formula(form), data=data)
-  M %*% betas
+  as.numeric(M %*% betas)
 }
 
+# check if output is the same as in the
+# frontier.fitted function
+# head(fitted(sf11x9, asInData = FALSE))
+# head(predict.sfa(sf11x9, db1))
